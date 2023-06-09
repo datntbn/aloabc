98381f1d7e8f8b59201845be9d7b283f0c4a3564
Dat <ntat.dat@gmail.com>
2023-06-09 21:09:11 +0700

    no message

---
+++ Bai tap/Bai tap/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace Bai_tap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Khoi tao 3 student
+
+            Student[] students = new Student[3];
+
+            for (int i = 1; i < 4; i++)
+            {
+            Student std = new Student();
+            Console.Write($"Nhap ten cua hoc sinh {i}: ");
+                string name = Console.ReadLine();
+
+            Console.Write($"Nhap diem Toan cua hoc sinh {i}: ");
+                string diemToan =  Console.ReadLine();
+
+            Console.Write($"Nhap diem Ly cua hoc sinh {i}: ");
+                string diemLy = Console.ReadLine();
+
+            Console.Write($"Nhap diem Hoa cua hoc sinh {i}: ");
+                string diemHoa = Console.ReadLine();
+
+            std.name = name;
+            std.diemToan = int.Parse(diemToan);
+            std.diemLy = int.Parse(diemLy);
+            std.diemHoa = int.Parse(diemHoa);
+            students[i] = std;
+            }
+
+            //In ra man hinh diem trung binh
+            for ( int i = 0; i < students.Length; i++)
+            {
+                students[i].ShowAvg();
+            }
+
+            //In ra man hinh sv co diem tb lon nhat
+             
+
+
+            //Xep loai sinh vien
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Bai tap/Bai tap/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class Student
+{
+    public string name = "";
+    public int diemToan;
+    public int diemLy;
+    public int diemHoa;
+    public float dtb;
+
+    public Student()
+    {
+    }
+
+    public Student(string name, int diemToan, int diemLy, int diemHoa)
+    {
+        this.name = name;
+        this.diemToan = diemToan;
+        this.diemLy = diemLy;
+        this.diemHoa = diemHoa;
+        //this.dtb = (diemToan + diemLy + diemHoa) / 3;
+    }
+
+    public void ShowAvg()
+    {
+        this.dtb = (diemHoa + diemLy + diemToan) / 3;
+        Console.WriteLine("Diem trung binh cua " + name + " la: " + dtb);
+    }
+}
